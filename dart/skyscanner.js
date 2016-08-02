383638dad3acc2f90d578b93dd99948646764df5
Catie Cook <catiecook888@gmail.com>
2016-08-02 11:14:12 -0600

    got the buttons to pop up but not linking correctly yet

--- dart/skyscanner.js
+++ dart/skyscanner.js
@@ -14,7 +14,7 @@ console.log($returnDate);
 var $url = "https://galvanize-cors-proxy.herokuapp.com/http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/US/USD/en-GB/" + $origin + "/anywhere/" + $departureDate + "/" + $returnDate + "?apiKey=ga774761977863345132258418049742&format=json";
 
 //link for the booking refferal
-var $link = "http://partners.api.skyscanner.net/apiservices/referral/v1.0/US/USD/en-GB/" + $origin + "/anywhere/" + $departureDate + "/" + $returnDate + "?apiKey=ga77476197786334";
+var $link = "http://partners.api.skyscanner.net/apiservices/referral/v1.0/US/USD/en-GB/" + $origin + "/anywhere/" + $departureDate + "/" + $returnDate + "?apiKey=ga77476197786334&format=json";
 
 // console.log($url);
 // console.log($link);
@@ -24,8 +24,9 @@ var $link = "http://partners.api.skyscanner.net/apiservices/referral/v1.0/US/USD
       for(var i=0; i<3; i++){
         //create new div with styles
         var $results = $("<div />", {
-          "class": "col s12 m3",
-          "style": "padding: 2vw",
+          "class": "row s12 m3",
+          "style": "padding: 2vw"
+
           //add event handler attr.data-url: 'link address'
         });
 
@@ -35,16 +36,14 @@ var $link = "http://partners.api.skyscanner.net/apiservices/referral/v1.0/US/USD
 
         $results.text("Destination: " + $destination + " " + "Price: $"+
         $minPrice  + " ");
-        $("#results").append($results);
-console.log($link);
-
-        
-          // console.log(data);
-          // $("#results").append($("<p>")).append($("<a>")).attr("href", $link).html("Book it") //in div created in loop, append a link to a new paragraph with the text "book it"
-
-
-        }
-  });
+        $("#results").append($results.append(
+          $("<div/>", {"class": "row s12 m3"}).append(
+            $("<p/>", {"class": "bookingLink"})
+            )
+          ).append($("<button>", {"class": "btn", "href": $link, "text": "Book It"}))
+        )
+      }
+    });
 });
 //see snippet in dev tools for making a sorted array of prices
 //ask about the branch
